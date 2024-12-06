85c545e0f9b14bcdd601c9dc8cd2b8e8bd1b26bc
ZenatyAh <zenatyah@gmail.com>
2024-12-06 20:31:44 +0200

    add Talwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
 )
