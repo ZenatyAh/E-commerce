ffe4379d4e81762dc22ee53bb559992c19c2f9b1
ZenatyAh <zenatyah@gmail.com>
2024-12-09 11:28:19 +0200

    build store with redux build reducers build action

---
+++ src/store/Categories/categoriesSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface ICategoriesState {
+    records: {id:number; title: string ; prefix : string; img: string}[];
+    loading: "idle" | "pending" | "secceeded" | "failed";
+    error: string | null;
+}
+
+const initialState: ICategoriesState = {
+    records: [],
+    loading: "idle",
+    error: null
+};
+
+const CategoriesSlice = createSlice({
+    name: "Categories",
+    initialState,
+    reducers: {}
+});
+
+export default CategoriesSlice.reducer;
